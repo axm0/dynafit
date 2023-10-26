c463e8277b14913641664e7f3c366aefe4048d66
Abdul Aziz Mohammed <abdul.aziz.mohammed@outlook.com>
2023-10-26 16:07:14 -0500

    added logout button fix

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { useAuth } from '../AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 function Logout() {
     const { setCurrentUser } = useAuth();
+    const navigate = useNavigate();
 
     const handleLogout = () => {
         setCurrentUser(null);
+        navigate('/');
     };
 
     return (
